4017bc8dd4d87c81ccf56e1de65fbb9d506ff1cb
Gabriel MR <gabrielmonesruiz@outlook.com>
2023-04-11 14:58:36 -0300

    actulización de archivos

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Public } from './auth/decorators/public.decorator';
 
+@Public()
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'DevOps Bootcamp!';
+    return 'DevOps Bootcamp! Grupo-3!';
   }
 }
